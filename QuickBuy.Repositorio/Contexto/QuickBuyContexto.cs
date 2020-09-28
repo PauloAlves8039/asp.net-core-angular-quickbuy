845514efd7edd02104e002e58dca966cf79e53da
PauloAlves8039 <pj38alves@gmail.com>
2020-09-27 21:37:00 -0300

    Implementação inicial de conexão com o banco de dados

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetoDeValor;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    /// <summary>
+    /// Classe responsável pelas configurações das entidades da aplicação no banco de dados.
+    /// </summary>
+    public class QuickBuyContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+        public QuickBuyContexto(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
