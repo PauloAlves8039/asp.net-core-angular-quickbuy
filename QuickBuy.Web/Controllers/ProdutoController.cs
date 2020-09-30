0ec4a6257fa8e6c1b895df05d17a3c8597ce2690
PauloAlves8039 <pj38alves@gmail.com>
2020-09-30 11:07:49 -0300

    Configuração inicial do ProdutoController

---
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Web.Controllers
+{
+    /// <summary>
+    /// Controlador responsável pelas ações referentes a entidade Produto.
+    /// </summary>
+    [Route("api/[Controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+    }
+}
