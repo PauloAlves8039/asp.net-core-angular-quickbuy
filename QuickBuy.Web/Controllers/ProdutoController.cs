42b71ad7de95349023effa6d6e2279f9933fe0a4
PauloAlves8039 <pj38alves@gmail.com>
2020-10-01 10:31:48 -0300

    Criando métodos para adicionar e retornar produtos

--- QuickBuy.Web/Controllers/ProdutoController.cs
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,5 +20,42 @@ namespace QuickBuy.Web.Controllers
         {
             _produtoRepositorio = produtoRepositorio;
         }
+
+        /// <summary>
+        /// Método para retornar todos os produtos.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Método para adicionar novo produto.
+        /// </summary>
+        /// <param name="produto">Instância da classe Produto para adição de novo registro.</param>
+        /// <returns>View com o produto adicionado.</returns>
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto) 
+        {
+            try
+            {
+                _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
     }
 }
