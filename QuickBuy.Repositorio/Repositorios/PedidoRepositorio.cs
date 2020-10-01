9046a18d2d2d3e81da30f5b7720e40accef6c0c5
PauloAlves8039 <pj38alves@gmail.com>
2020-10-01 15:09:37 -0300

    Implementando alterações para testar WEB API

--- QuickBuy.Repositorio/Repositorios/PedidoRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/PedidoRepositorio.cs
@@ -1,13 +1,16 @@
 ﻿using QuickBuy.Dominio.Contratos;
 using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
     /// <summary>
     /// Classe de repositório base para regra de negócio referente a entidade Pedido.
     /// </summary>
-    public class PedidoRepositorio: BaseRepositorio<Pedido>, IPedidoRepositorio
+    public class PedidoRepositorio : BaseRepositorio<Pedido>, IPedidoRepositorio
     {
-
+        public PedidoRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
+        {
+        }
     }
 }
