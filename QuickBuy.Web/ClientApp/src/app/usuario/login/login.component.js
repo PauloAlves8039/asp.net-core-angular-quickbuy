157df4167ecf8316d70674e6cf0080d0ca6fd2c1
PauloAlves8039 <pj38alves@gmail.com>
2020-10-05 09:13:48 -0300

    Implementação inicial do componente login

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.js
@@ -0,0 +1,7 @@
+/**
+ * @file login.component.ts
+ * @author: Paulo Alves
+ * @description: componente responsável pelo login do usuário na aplicação.
+ * @version 1.0.1 (05/10/2020)
+ */
+//# sourceMappingURL=login.component.js.map
---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -0,0 +1,8 @@
+/**
+ * @file login.component.ts
+ * @author: Paulo Alves
+ * @description: componente responsável pelo login do usuário na aplicação.
+ * @version 1.0.1 (05/10/2020)
+ */
+
+
